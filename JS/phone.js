ac91898a6d82b32130704814f49db9dbd330a96e
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-07-05 01:20:14 +0600

    details section added

--- JS/phone.js
+++ JS/phone.js
@@ -29,7 +29,7 @@ function phoneDisplay(products, limit) {
             <p class="text-lg">If a dog chews shoes whose shoes does he choose?</p>
             <h2 class=" text-2xl font-bold">$999</h2>
             <div class="card-actions">
-                <button onclick="loadPhoneDetails('${product.slug}')" class="btn bg-blue-500 hover:bg-blue-600 text-white">Show Details</button>
+                <button  onclick="loadPhoneDetails('${product.slug}')" class="btn bg-blue-500 hover:bg-blue-600 text-white">Show Details</button>
             </div>
         </div>
         `
@@ -42,7 +42,7 @@ const defaultPhoneSeeAll = () => {
     isLoading(true);
     const inputFieldValue = document.getElementById('input-field').value;
     console.log(inputFieldValue);
-    defaultPhonesLoad(inputFieldValue);
+    inputFieldValue ? defaultPhonesLoad(inputFieldValue) : defaultPhonesLoad('iphone');
 }
 
 const processSearch = (limit) => {
@@ -72,6 +72,21 @@ const loadPhoneDetails = async (id) => {
 }
 const phoneDetailsDisplay = (phone) => {
 
+    document.getElementById("modalImage").setAttribute('src', phone.image);
+    const detailsDiv = document.getElementById('description-details');
+    detailsDiv.innerHTML = `
+        <h2 id="name" class="card-title text-3xl font-bold">${phone.name}</h2>
+        <p class="text-base">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+        <p class="text-xl"><span class="font-bold">Storage:</span> ${phone?.mainFeatures?.storage}</p>
+        <p class="text-xl"><span class="font-bold">Display Size:</span> ${phone?.mainFeatures?.displaySize}</p>
+        <p class="text-xl"><span class="font-bold">Chipset:</span> ${phone?.mainFeatures?.chipSet}</p>
+        <p class="text-xl"><span class="font-bold">Memory:</span> ${phone?.mainFeatures?.memory}</p>
+        <p class="text-xl"><span class="font-bold">Slug:</span> ${phone?.slug}</p>
+        <p class="text-xl"><span class="font-bold">Release data:</span> ${phone?.releaseDate || "Recent"}</p>
+        <p class="text-xl"><span class="font-bold">Brand:</span> ${phone?.brand}</p>
+        <p class="text-xl"><span class="font-bold">GPS:</span> ${phone?.others?.GPS || "No GPS"}</p>
+    `
+    my_modal.showModal()
 }
 
 const isLoading = (value) => {
