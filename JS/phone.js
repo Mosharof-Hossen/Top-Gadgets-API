2e3730f073919874b89364f7dd3a40529518aee9
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-07-04 16:48:47 +0600

    nav and banner content added

--- JS/phone.js
+++ JS/phone.js
@@ -1,1 +1,12 @@
-console.log("phone");
+const phonesLoad = async ()=> {
+    const url = 'https://openapi.programming-hero.com/api/phones?search=iphone';
+    const res = await fetch(url);
+    const phones = await res.json();
+    console.log(phones.data);
+}
+
+function phoneDisplay(params) {
+    
+}
+
+phonesLoad()
